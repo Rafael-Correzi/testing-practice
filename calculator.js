d88bc585ba39fd181f1b3191fd20370afaa1e473
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-02-26 18:58:00 -0300

    Add subtractio operation to calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 const calculator = {
   add: function(a, b) {
     return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
   }
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ import { calculator } from "./calculator.js";
 it('Add to 25', () => {
   expect(calculator.add(20, 5)).toBe(25);
 });
+
+it('Add to 25', () => {
+  expect(calculator.subtract(20, 5)).toBe(15);
+});
