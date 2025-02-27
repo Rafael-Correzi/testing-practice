4a6991667b6a71700262a2440c2b0b45d363766f
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-02-27 14:11:54 -0300

    Delete config folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }}, {ignores: ["**/*.test.js", "**/*.spec.js"]},
+  {languageOptions: { globals: globals.browser }}, {ignores: ["**/*.test.js", "**/*.spec.js", "babel.config.js"]},
   pluginJs.configs.recommended,
 ];
